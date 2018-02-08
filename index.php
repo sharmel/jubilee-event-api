497f730f8a6485ab4a8ebd16ae19fc616c726a51
Capify <capify@Capifys-Mac-mini.local>
2018-02-08 13:23:38 +0000

    update package

--- index.php
+++ index.php
@@ -3,48 +3,48 @@
 require 'vendor/autoload.php';
 include 'bootstrap.php';
 
-use Chatter\Models\Message;
-use Chatter\Middleware\Logging as ChatterLogging;
-use Chatter\Middleware\Authentication as ChatterAuth;
-use Chatter\Middleware\FileFilter;
-use Chatter\Middleware\FileMove;
-use Chatter\Middleware\ImageRemoveExif;
+use Event\Models\Event;
+use Event\Middleware\Logging as EventLogging;
+use Event\Middleware\Authentication as EventAuth;
+use Event\Middleware\FileFilter;
+use Event\Middleware\FileMove;
+use Event\Middleware\ImageRemoveExif;
 
 $app = new \Slim\App();
-$app->add(new ChatterAuth());
-$app->add(new ChatterLogging());
+$app->add(new EventAuth());
+$app->add(new EventLogging());
 
 $app->group('/v1' , function() {
-    $app->group('/messages', function () {
+    $app->group('/events', function () {
         $this->map(['GET'], '', function ($request, $response, $args) {
-            $_message = new Message();
+            $_event = new Event();
 
-            $messages = $_message->all();
+            $events = $_event->all();
 
             $payload = [];
-            foreach($messages as $_msg) {
+            foreach($events as $_msg) {
                 $payload[$_msg->id] = $_msg->output();
             }
 
             return $response->withStatus(200)->withJson($payload);
-        })->setName('get_messages');
+        })->setName('get_events');
     });
 });
 
 $app->group('/v2' , function() {
-    $app->group('/messages', function () {
+    $app->group('/events', function () {
         $this->map(['GET'], '', function ($request, $response, $args) {
-            $_message = new Message();
+            $_event = new Event();
 
-            $messages = $_message->all();
+            $events = $_event->all();
 
             $payload = [];
-            foreach($messages as $_msg) {
+            foreach($events as $_msg) {
                 $payload[$_msg->id] = $_msg->output();
             }
 
             return $response->withStatus(200)->withJson($payload);
-        })->setName('get_messages');
+        })->setName('get_events');
     });
 });
 
@@ -53,36 +53,35 @@ $app->group('/v2' , function() {
     $move   = new FileMove();
     
     $this->map(['POST'], '', function ($request, $response, $args) {
-        $_message = $request->getParsedBodyParam('message', '');
+        $_event = $request->getParsedBodyParam('event', '');
 
-        $message = new Message();
-        $message->body = $_message;
-        $message->user_id = $request->getAttribute('user_id');
-        $message->image_url = $request->getAttribute('filename');
-        $message->save();
+        $event = new Event();
+        $event->body = $_event;
+        $event->user_id = $request->getAttribute('user_id');
+        $event->image_url = $request->getAttribute('filename');
+        $event->save();
 
-        if ($message->id) {
-            $payload = ['message_id' => $message->id,
-                'message_uri' => '/messages/' . $message->id,
-                'image_url' => $message->image_url
+        if ($event->id) {
+            $payload = ['event_id' => $event->id,
+                'event_uri' => '/events/' . $event->id,
+                'image_url' => $event->image_url
             ];
             return $response->withStatus(201)->withJson($payload);
         } else {
             return $response->withStatus(400);
         }
-    })->add($filter)->add($removeExif)->add($move)->setName('create_messages');
+    })->add($filter)->add($removeExif)->add($move)->setName('create_events');
 
-    $this->delete('/{message_id}', function ($request, $response, $args) {
-        $message = Message::find($args['message_id']);
-        $message->delete();
+    $this->delete('/{event_id}', function ($request, $response, $args) {
+        $event = Event::find($args['event_id']);
+        $event->delete();
 
-        if ($message->exists) {
+        if ($event->exists) {
             return $response->withStatus(400);
         } else {
             return $response->withStatus(204);
         }
-    })->setName('delete_messages');
-});
+    })->setName('delete_events');
 
 // Run app
 $app->run();
